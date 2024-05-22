3b119ba4c73ea81a6b65998c330d5c81906433d7
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 10:31:04 -0700

    Fixed task 0

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>"
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_t(n):
     """ Displays a number """
     return render_template('5-number.html')

99105c0f15e27ac2e62c031129d783cd05869453
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 09:32:11 -0700

    Added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Script to start flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/",strict_slashes=False)
+def hello_hbnb():
+    """ returns text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb",strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB!"
+
+
+@app.route("//c/<text>", strict_slashes=False)
+def c(text):
+    """ returns the value of text """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pyt(text="is cool"):
+    """ returns the value of python """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+	
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")

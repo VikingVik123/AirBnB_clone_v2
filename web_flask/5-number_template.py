5ba709720c4bad6a2bbb6b10aac9d983d9e3319f
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 10:24:10 -0700

    Added task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Script to start flask
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ returns text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB!"
+
+
+@app.route("//c/<text>", strict_slashes=False)
+def c(text):
+    """ returns the value of text """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pyt(text="is cool"):
+    """ returns the value of python """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """Displays a number"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>"
+def number_t(n):
+    """ Displays a number """
+    return render_template('5-number.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")

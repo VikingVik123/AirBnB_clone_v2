6f10751a88fea561414fbcca8cfd076938183996
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 08:00:06 -0700

    Fixed tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,16 +6,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
-@app.route("/hbnb",strict_slashes=False)
 
+@app.route("/", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """ returns text """
     return "Hello HBNB!"
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ returns the HBNB text"""
     return "HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,23 +6,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
 
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ returns text"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns hbnb"""
     return "HBNB!"
+
 
 @app.route("//c/<text>", strict_slashes=False)
 def c(text):
     """ returns the value of text """
     text = text.replace("_", " ")
     return "C {}".format(text)
-	
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")

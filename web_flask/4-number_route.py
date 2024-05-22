a45de036168ceff1e57b93067808dd890ded0783
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 10:09:20 -0700

    Fixed task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """
 Script to start flask
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
 
@@ -33,14 +40,11 @@ def pyt(text="is cool"):
     return "Python {}".format(text)
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Displays a number"""
-    if (n not int):
-        raise ValueError
-    else:
-        return "{} is a number".format(n)
-	
+    return "{} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")

0ec7f98e051157b59b04860ed2afb17f06263096
VikingVik123 <vodimmegwa@gmail.com>
2024-05-22 06:44:14 -0700

    Fixed task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+@app.route("/", strict_slashes=False)
 
 def hello_hbnb():
-        return "<p>Hello HBNB!</p>"
+        return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(debug=True)

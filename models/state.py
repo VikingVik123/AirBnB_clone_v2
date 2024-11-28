340861d9680ef78f3f1e9943f1dd5d791fa96ba0
VikingVik123 <vodimmegwa@gmail.com>
2024-11-28 12:33:53 +0100

    updated models.city

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ The state class
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """

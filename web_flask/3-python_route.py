203adec2c9078ccfc4dd91261576c341c1471dfb
VikingVik123 <vodimmegwa@gmail.com>
2025-05-13 13:48:53 +0100

    fix/fixed the PEP8 on 3-python_route file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pyt(text="is cool"):
